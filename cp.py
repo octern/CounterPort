769cefde3df39f41cf5b8009614fd0ec3a44e4c7
octern-zipslime <non@mcohn.net>
2021-10-04 12:26:31 -0700

    seems fully working, not checked for correctness

--- cp.py
+++ cp.py
@@ -22,26 +22,26 @@ def read_transactions(transactions):
 		operations[datetime.datetime.now().date() - datetime.timedelta(days=3)] = 0
 	return operations
 
-
+debug = 0
 #symbols = [sys.argv[0]]
 
-symbols = ["AAPL"]
+symbols = ["F"]
 
 #pdb.set_trace()
 operations = {}
 transactions = pd.read_csv("ubs_transactions.csv")
 operations = read_transactions(transactions)
 dateList = []
-for d in sorted(operations.keys())[0:5]:
+for d in sorted(operations.keys()):
 	dateList.append(d)
 
 prices = {}
 for date in dateList:
 	prices[date] = dp.download_one_price(date=date, symbol=symbols[0])[0]
-	print(prices[date])
+	print("{}: {}, {}".format(date.strftime("%Y-%m-%d"), round(prices[date], 2), round(operations[date])))
 
 cpValues = {}
-cpValues[dateList[0]] = operations[dateList[0]]
+cpValues[dateList[0]] = round(operations[dateList[0]])
 for i in range(1,len(dateList)):
 	oldDate = dateList[i-1]
 	newDate = dateList[i]
@@ -49,9 +49,20 @@ for i in range(1,len(dateList)):
 	priceChange = prices[newDate] / prices[oldDate]
 	valueBeforeTransaction = cpValues[oldDate] * priceChange
 	valueAfterTransaction = valueBeforeTransaction + operations[newDate]
-	cpValues[newDate] = valueAfterTransaction
+	cpValues[newDate] = round(valueAfterTransaction)
 	review = {"oldDate":oldDate, "newDate":newDate, "oldValue":oldValue, "oldPrice":prices[oldDate], "newPrice":prices[newDate], "transaction":operations[newDate], "valueBeforeTransaction":valueBeforeTransaction, "valueAfterTransaction":valueAfterTransaction}
-	for k in sorted(review.keys()):
-		print("{}: {}".format(k, review[k]))
-	print("---")
-	pdb.set_trace()
+	if debug:
+		for k in sorted(review.keys()):
+			print("{}: {}".format(k, review[k]))
+		print("---")
+#		pdb.set_trace()
+
+startValue = cpValues[dateList[0]]
+endValue = cpValues[dateList[-1]]
+growth = round(endValue - startValue)
+growthProp = round(endValue / startValue, 2)
+ops = sum(operations.values()) - operations[dateList[0]]
+print("start value {}: ".format(dateList[0].strftime("%Y-%m-%d")), startValue)
+print("final value {}: ".format(dateList[-1].strftime("%Y-%m-%d")), endValue)
+print("growth factor: {}".format(growthProp))
+print("inputs/outputs: {} ({} of total growth)".format(round(ops), round(ops/growth, 2)))
